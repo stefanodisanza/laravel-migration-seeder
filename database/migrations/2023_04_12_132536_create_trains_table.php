0e2309c0c3163a4cbf8930407519c94b9ddae94a
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-04-12 15:52:55 +0200

    first commit

---
+++ database/migrations/2023_04_12_132536_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->string('Codice Treno');
+            $table->integer('Numero Carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
