cb690a17d4be503c80476149a0b119219ab6ee00
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-04-13 16:54:23 +0200

    FEAT seeder

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('trains')->insert([
+                'Azienda' => $faker->company,
+                'Stazione di partenza' => $faker->city,
+                'Stazione di arrivo' => $faker->city,
+                'Orario di partenza' => $faker->time($format = 'H:i:s', $max = 'now'),
+                'Orario di arrivo' => $faker->time($format = 'H:i:s', $max = 'now'),
+                'Codice Treno' => $faker->bothify('??###'),
+                'Numero Carrozze' => $faker->numberBetween($min = 1, $max = 20),
+                'In orario' => $faker->boolean,
+                'Cancellato' => $faker->boolean
+            ]);
+        }
+    }
+}
